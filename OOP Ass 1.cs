dd48d3def152464290147a760e7450f574c04602
RustomBundalian <rustombundalian@gmail.com>
2022-04-27 12:04:24 -0700

    First Commit

---
+++ OOP Ass 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Ass_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello! I am Rustom Bundalian, it is nice to meet you! :)");
+        }
+    }
+}
